babc49b393ffe44c2714bb11edd4476523eb4bb6
Pabloino <pablo-ino@hotmail.com>
2018-09-30 20:58:57 -0300

    Implemento la funcion convertions

---
+++ src/convertions.clj
@@ -0,0 +1,5 @@
+(ns convertions
+  (require [clojure.data.json :as json]))
+
+(defn json_to_map [j] (json/read-str j))
+(defn map_to_json [j] (json/write-stir j))
--- test/unit_testing.clj
+++ test/unit_testing.clj
@@ -1,6 +1,7 @@
 (ns unit_testing
   (:require [clojure.test :refer :all]
-    [translations :refer :all]))
+    [translations :refer :all]
+    [convertions :refer :all]))
 
 (deftest translate-some-month
   (testing "Test translations applied to SEPTEMBER"
