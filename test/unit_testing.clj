2e9faf6804187f49cdd166c0ad78aa1895f9bf1d
Pabloino <pablo-ino@hotmail.com>
2018-09-30 23:44:30 -0300

    Agrego el test-access-to-map-value al unit testing

--- test/unit_testing.clj
+++ test/unit_testing.clj
@@ -1,7 +1,8 @@
 (ns unit_testing
   (:require [clojure.test :refer :all]
     [translations :refer :all]
-    [convertions :refer :all]))
+    [convertions :refer :all]
+    [clojure.data.json :as json]))
 
 (deftest translate-some-month
   (testing "Test translations applied to SEPTEMBER"
@@ -14,3 +15,7 @@
 (deftest convert-to-empty-vector-string
   (testing "Test convert empty vector to json"
   (is (= (map_to_json []) "[]"))))
+
+(deftest test-access-to-map-value
+  (testing "Test to convert json to map and access to any value"
+  (is (= (get (json_to_map "{\"a\":4,\"b\":8,\"c\":5}") "b") 8))))
