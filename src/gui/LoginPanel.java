9d18f11f6c0f60b18135fd979d8433d7cb411ff9
Pabloino <pablo-ino@hotmail.com>
2018-11-08 16:42:58 -0300

    Adjusting some elements of login screen

--- src/gui/LoginPanel.java
+++ src/gui/LoginPanel.java
@@ -12,10 +12,14 @@ public class LoginPanel {
         panel.setLayout(null);
 
         JButton iniciar = new JButton("Iniciar");
-        iniciar.setBounds(348, 364, 100, 30);
+        iniciar.setBounds(348, 410, 100, 30);
+
+        JButton acercaDe = new JButton("Info");
+        //acercaDe.setBounds(690, 525, 70, 20);
+        acercaDe.setBounds(690, 20, 70, 20);
 
         JComboBox rolSelect = new JComboBox();
-        rolSelect.setBounds(580, 40, 170, 25);
+        rolSelect.setBounds(350, 360, 190, 25);
         rolSelect.addItem("Cajero");
         rolSelect.addItem("Supervisor");
 
@@ -25,30 +29,29 @@ public class LoginPanel {
         JLabel passText = new JLabel("Contraseña:");
         passText.setBounds(230, 290, 330, 20);
 
+        JLabel rolText = new JLabel("Iniciar como:");
+        rolText.setBounds(230, 362, 100, 20);
+
         JTextField userBox = new JTextField();
         userBox.setBounds(220, 250, 350, 30);
 
         JTextField passBox = new JTextField();
         passBox.setBounds(220, 315, 350, 30);
 
-        JTextArea aboutText = new JTextArea("Trabajo Práctico:" + 
+        JLabel aboutText = new JLabel("Trabajo Práctico:" + 
         " Técnicas de Diseño - 2do Cuatrimestre 2018");
-        aboutText.setBounds(200, 505, 390, 20);
-        aboutText.setOpaque(false);
-
-        JTextArea contributors = new JTextArea("Grupo 4:" + 
-        " Llauró, Manuel - Inoriza, Pablo - Arturi, Augusto - Blanco, Sebastian Ezequiel");
-        contributors.setBounds(130, 525, 530, 20);
-        contributors.setOpaque(false);        
+        aboutText.setBounds(180, 525, 440, 20);
+        aboutText.setOpaque(false);      
 
         panel.add(iniciar);
+        panel.add(acercaDe);
         panel.add(userText);
         panel.add(passText);
         panel.add(userBox);
         panel.add(passBox);
         panel.add(rolSelect);
+        panel.add(rolText);
         panel.add(aboutText);
-        panel.add(contributors);
     }
     
     public JPanel getPanel(){
