705fd0bb6de36e0b0df6f8956fa571c2ca2f8027
Pabloino <pablo-ino@hotmail.com>
2018-11-18 18:46:40 -0300

    Adding the shopping list screen for supervisor

--- src/gui/StartGui.java
+++ src/gui/StartGui.java
@@ -29,7 +29,8 @@ public class StartGui {
         StartGui gui = new StartGui();
         //Panel login = new LoginPanel(gui.getFrame());
         //Panel login = new BoxesPanel(gui.getFrame());
-        Panel login = new ShoppingListCashier(gui.getFrame());
+        //Panel login = new ShoppingListCashier(gui.getFrame());
+        Panel login = new ShoppingListSupervisor(gui.getFrame());
 
         gui.getFrame().getContentPane().add(login.getPanel());
         gui.refreshScreen();
